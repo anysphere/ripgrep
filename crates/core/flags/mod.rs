12e59c1bc7dedff7b222a9599c207e3b10737bb3
hanpengg <hanpeng@anysphere.co>
2025-11-14 18:15:04 -0500

    resolve merge conflicts

--- crates/core/flags/mod.rs
+++ crates/core/flags/mod.rs
@@ -35,13 +35,8 @@ pub(crate) use crate::flags::{
         },
     },
     hiargs::HiArgs,
-<<<<<<< HEAD
     lowargs::{GenerateMode, Mode, SearchMode, SortModeKind, SpecialMode},
-    parse::{parse, ParseResult},
-=======
-    lowargs::{GenerateMode, Mode, SearchMode, SpecialMode},
     parse::{ParseResult, parse},
->>>>>>> 15.1.0
 };
 
 mod complete;
